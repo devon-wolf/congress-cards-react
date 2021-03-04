1608ca70986dc6d699831c2c42b5f7cd2b231d6f
Devon Wolfkiel <devonwolf@tutanota.com>
2021-03-04 11:53:06 -0800

    Add chamber dropdown to search page

--- src/search/SearchForm.js
+++ src/search/SearchForm.js
@@ -4,7 +4,17 @@ export default class SearchForm extends Component {
 	render() {
 		return (
 			<form>
-				
+				<label>
+					<span>Search which chamber?</span>
+					<select 
+					value={this.props.chamber}
+					onChange={this.props.handleChamberChange}
+					>
+						<option value=""></option>
+						<option value="house">House of Representatives</option>
+						<option value="senate">Senate</option>
+					</select>
+				</label>
 			</form>
 		)
 	}
--- src/search/SearchPage.js
+++ src/search/SearchPage.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import SearchForm from './SearchForm.js'
 
 export default class SearchPage extends Component {
+	state = {
+		chamber: ''
+	}
+
 	render() {
+		// console.log(this.state);
 		return (
 			<main>
 				<h1>SEARCH</h1>
+				<SearchForm 
+					chamber={this.state.chamber}
+					handleChamberChange={e => this.setState({chamber: e.target.value})}
+				/>
 			</main>
 		)
 	}
