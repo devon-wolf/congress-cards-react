e0fa5c211af0dabd00691fe56d89cc9cde97e30c
Devon Wolfkiel <devonwolf@tutanota.com>
2021-03-04 16:20:00 -0800

    Collected congressfolk are now added to state cumulatively on click

--- src/search/SearchList.js
+++ src/search/SearchList.js
@@ -3,7 +3,18 @@ import SearchItem from './SearchItem.js'
 import style from './Search.module.css'
 
 export default class SearchList extends Component {
+	state = {
+		collection: []
+	}
+
+	handleCollectClick = (item) => {
+		let collection = [];
+		collection.push(...this.state.collection, item);
+		this.setState({ collection });
+	}
+
 	render() {
+		console.log(this.state);
 		return (
 			<ul className={style.results}>
 				{this.props.items.map(item => 
@@ -11,7 +22,7 @@ export default class SearchList extends Component {
 				name={item.first_name + ' ' + item.last_name} 
 				state={item.state} key={item.id + item.party}
 				party={item.party}
-				handleCollectClick={e => console.log(item)}
+				handleCollectClick={() => this.handleCollectClick(item)}
 				/>)}
 			</ul>
 		)
